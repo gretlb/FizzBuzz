0ff4ff7149175998c42706e59afe61da7b557aff
gretlb <gretl.baghdadi@mail.utoronto.ca>
2024-05-17 13:22:50 -0400

    tast 4

---
+++ src/Multiples.java
@@ -0,0 +1,23 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class Multiples {
+    public static void main(String[] args) {
+        extracted();
+    }
+
+    private static void extracted() {
+        int limit  = 1000;
+
+        int i = 0;
+        while (i < 101) {
+
+            FizzBuzz.doFizzBuzz(5);
+
+            i = i + 1;
+        }
+        System.out.println("");
+    }
+
+
+//    2 helper fxsns
+}
